b2639c6832626199bbe490dcd5405cc825670577
Spencer Lee <lesp@seas.upenn.edu>
2013-01-20 16:28:50 -0500

    Added basic error checking to calculator.lua
    -Check to make sure first and second arguments are actually numbers
    -Moved helper functions to be above main function

--- calculator.lua
+++ calculator.lua
@@ -8,26 +8,39 @@ multiply=3
 divide=4;
 modulus=5;
 
-function calculate(first,second,operator)
-  --Code goes in here
-end
-
 function add_cal(first,second)
-  --Code goes in here
+  if not type(first)=='number' or not type(second)==number then
+    error('Cannot add values which are not numbers');
+  end
 end
 
 function subtract_cal(first,second)
-  --Code goes in here
+  if not type(first)=='number' or not type(second)==number then
+    error('Cannot subtract values which are not numbers');
+  end
 end
 
 function multiply_cal(first,second)
-  --Code goes in here
+  if not type(first)=='number' or not type(second)==number then
+    error('Cannot multiply values which are not numbers');
+  end
 end
 
 function divide_cal(first,second)
-  --Code goes in here
+  if not type(first)=='number' or not type(second)==number then
+    error('Cannot divide values which are not numbers');
+  end
 end
 
 function modulus_cal(first,second)
+  if not type(first)=='number' or not type(second)==number then
+    error('Cannot take modulus of values which are not numbers');
+  end
+end
+
+function calculate(first,second,operator)
   --Code goes in here
+  if not type(first)=='number' or not type(second)==number then
+    error('Cannot add values which are not numbers');
+  end
 end
