77bf33ca53b0a052681593060ee73bf13df19679
Spencer Lee <lesp@seas.upenn.edu>
2013-01-20 15:05:15 -0500

    Initial commit

---
+++ calculator.lua
@@ -0,0 +1,15 @@
+module(...,package.seeall);
+
+require('math')
+
+add=1;
+subtract=2;
+multiply=3
+divide=4;
+modulus=5;
+sin=6;
+cos=7;
+
+function calculate(first,second,operator)
+  --Code goes in here
+end
