4044f940f7e55782803a843151560cebcb378572
Spencer Lee <lesp@seas.upenn.edu>
2013-01-20 16:05:41 -0500

    Added function documentation
    -Modified calculator.lua to have a luadoc style comment for calculator

--- calculator.lua
+++ calculator.lua
@@ -1,16 +1,25 @@
-module(..., package.seeall);
+module(..., package.seeall); --Declare this file as a module
 
-require('math')
+require('math') --Requiring math library because lazy
 
+--Declare operations which can be taken by the calculator
 add=1;
 subtract=2;
 multiply=3
 divide=4;
 modulus=5;
 
+---
+--The main function which is to be called by outside modules
+--@param first The first argument to the calculator
+--@param second The second argument to the calculator
+--@param operator The operation to be performed (one as defined above)
+--@return A number representing the answer calculated
 function calculate(first,second,operator)
   --Code goes in here
 end
+
+--Helper functions so as to not be trapped by one implementation
 
 function add_cal(first,second)
   --Code goes in here
